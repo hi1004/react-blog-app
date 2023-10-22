5adf3697492ca4521946e2893957c5ff843b5cd7
hi1004 <kipo09@naver.com>
2023-10-22 20:17:15 +0900

    style: header-styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,52 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Logo</Link>
+    <header className="flex justify-between border-b-[1px] items-center border-b-slate-300 px-[10px] py-7 fixed w-full z-10 top-0 left-0">
+      <Link to="/" className="text-3xl font-bold">
+        <span className="text-blue-600">React</span> Blog
+      </Link>
       <nav>
-        <ul>
-          <li>
-            <NavLink to="posts">投稿記事</NavLink>
+        <ul className="flex gap-4">
+          <li className="relative">
+            <NavLink
+              className={({ isActive }) =>
+                `header__line--after ${
+                  isActive
+                    ? 'text-blue-600 pointerhover:hover:text-blue-800'
+                    : 'text-gray-400 pointerhover:hover:text-gray-800'
+                }`
+              }
+              to="posts"
+              end
+            >
+              投稿記事
+            </NavLink>
+          </li>
+          <li className="relative">
+            <NavLink
+              className={({ isActive }) =>
+                `header__line--after ${
+                  isActive
+                    ? 'text-blue-600 pointerhover:hover:text-blue-800'
+                    : 'text-gray-400 pointerhover:hover:text-gray-800'
+                }`
+              }
+              to="posts/new"
+            >
+              投稿
+            </NavLink>
           </li>
           <li>
-            <NavLink to="posts/new">投稿</NavLink>
-          </li>
-          <li>
-            <NavLink to="profile">プロフィール</NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? 'text-blue-600 pointerhover:hover:text-blue-800'
+                  : 'text-gray-400 pointerhover:hover:text-gray-800'
+              }
+              to="profile"
+            >
+              プロフィール
+            </NavLink>
           </li>
         </ul>
       </nav>
